26466c6af1fcfcaff8ef824dd956c9efc06edde6
Apoorv Saxena <112375644+apoorvsxna@users.noreply.github.com>
2025-05-06 14:36:35 +0000

    testing

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ import bencode from 'bencode';
 
 const torrent = bencode.decode(fs.readFileSync('../wywh.torrent'));
 console.log(Buffer.from(torrent.announce, 'hex').toString('utf8'));
+
+console.log('testing');
